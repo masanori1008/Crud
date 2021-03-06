a30a674cd5564dd27f34c1f6ac00714d544f6661
agaday95 <phamminhduc1008@gmail.com>
2021-03-06 22:00:49 +0700

    abc

---
+++ Model/ResourceModel/Post/Collection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AHT\Crud\Model\ResourceModel\Post;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+	protected $_idFieldName = 'post_id';
+	protected $_eventPrefix = 'aht_crud_post_collection';
+	protected $_eventObject = 'post_collection';
+
+	/**
+	 * Define resource model
+	 *
+	 * @return void
+	 */
+	protected function _construct()
+	{
+		$this->_init('AHT\Crud\Model\Post', 'AHT\Crud\Model\ResourceModel\Post');
+	}
+
+}
